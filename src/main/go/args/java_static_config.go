456104e0c496395ed5e6e04b69d86977d9150f21
Mateusz "Serafin" Gajewski <github@wendigo.pl>
2026-08-15 23:51:33 +0200

    Remove workaround for JDK-8369506 from JDK26+

--- src/main/go/args/java_static_config.go
+++ src/main/go/args/java_static_config.go
@@ -25,9 +25,9 @@ var jvmSpecificConfig = map[string][]string{
 	"23": {rewriteBytecodes, allowDynamicAgentLoading, vectorApiIncubating},
 	"24": {rewriteBytecodes, allowDynamicAgentLoading, vectorApiIncubating, allowUnsafeUsage, allowNativeUsage},
 	"25": {rewriteBytecodes, allowDynamicAgentLoading, vectorApiIncubating, allowUnsafeUsage, allowNativeUsage, compactObjectHeaders},
-	"26": {rewriteBytecodes, allowDynamicAgentLoading, vectorApiIncubating, allowUnsafeUsage, allowNativeUsage, compactObjectHeaders},
-	"27": {rewriteBytecodes, allowDynamicAgentLoading, vectorApiIncubating, allowUnsafeUsage, allowNativeUsage},
-	"28": {rewriteBytecodes, allowDynamicAgentLoading, vectorApiIncubating, allowUnsafeUsage, allowNativeUsage},
+	"26": {allowDynamicAgentLoading, vectorApiIncubating, allowUnsafeUsage, allowNativeUsage, compactObjectHeaders},
+	"27": {allowDynamicAgentLoading, vectorApiIncubating, allowUnsafeUsage, allowNativeUsage},
+	"28": {allowDynamicAgentLoading, vectorApiIncubating, allowUnsafeUsage, allowNativeUsage},
 }
 
 func onArm64Only(option string) string {
